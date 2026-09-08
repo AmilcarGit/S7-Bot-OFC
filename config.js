a8ff73f5de917d6f2b42ce05f1b63469af4eda57
AMILCAR OFICIAL <amilkarurquiagaramos1@gmail.com>
2026-09-08 18:01:00 -0500

    Crear config.js

---
+++ config.js
@@ -0,0 +1,23 @@
+export default {
+  // Nombre visible del bot
+  botName: "S7-Bot-OFC",
+
+  // Número del dueño/dueños del bot (formato: 549XXXXXXXXXX sin "+")
+  owner: ["50000000000"],
+
+  // Prefijo de los comandos (puedes usar varios: ["!", ".", "/"])
+  prefix: [".", "!", "/"],
+
+  // Imagen que se envía junto al menú
+  menuImage: "https://i.postimg.cc/ZnR16vzL/file-0000000025b4820e8b55b1aff4a014e9.png",
+
+  // Número que usará el bot para vincularse por código de 8 dígitos
+  // Se pide por consola al iniciar si está vacío
+  phoneNumber: "",
+
+  // Mensaje de bienvenida en grupos
+  welcome: true,
+
+  // Antilink activado por defecto en grupos nuevos
+  antilinkDefault: false,
+};
