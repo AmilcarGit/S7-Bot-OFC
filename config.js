c20bfa7ce14e0ef40cf281e7676f7d22f6b40030
AMILCAR OFICIAL <amilkarurquiagaramos1@gmail.com>
2026-09-09 15:05:47 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,22 +1,19 @@
 export default {
-  // Nombre visible del bot
   botName: "S7-Bot-OFC",
 
-  // Número del dueño/dueños del bot (formato: código de país + número, sin "+")
   owner: ["51910227479", "269715926691844"],
 
-  // Prefijo de los comandos (puedes usar varios: ["!", ".", "/"])
   prefix: [".", "!", "/"],
 
-  // Imagen que se envía junto al menú
   menuImage: "https://i.postimg.cc/ZnR16vzL/file-0000000025b4820e8b55b1aff4a014e9.png",
 
-  // Número que usará el bot para vincularse por código de 8 dígitos
   phoneNumber: "",
 
-  // Mensaje de bienvenida en grupos (valor por defecto si no se usa .welcome on/off)
   welcome: true,
 
-  // Antilink activado por defecto en grupos nuevos
   antilinkDefault: false,
+
+  orbitApiKey: process.env.ORBIT_API_KEY || "ORBIT-4096939993",
+
+  orbitIp: process.env.ORBIT_IP || "10.174.52.60",
 };
